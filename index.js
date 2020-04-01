805ec423766c4f7f21c75ada040a31e6c580d8b1
Unknown <daniel135790@gmail.com>
2020-04-01 14:38:31 +0300

    Implemented dynamic users list

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 const ws = require('ws');
 const express = require('express');
-const {v4: uuidv4} = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const PORT = process.env.PORT || 8080;
 
 const server = express()
-  .use((req, res) => res.send('Hello World'))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+    .use((req, res) => res.send('Hello World'))
+    .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-const wss = new ws.Server({server});
+const wss = new ws.Server({ server });
 
 const clients = new Set();
 
-const attachMessageId = (rawMessage) => {
-    const parsedMessage = JSON.parse(rawMessage);
+const attachMessageId = (parsedMessage) => {
     const messageUuid = uuidv4();
 
     const targetMessage = {
@@ -25,22 +24,61 @@ const attachMessageId = (rawMessage) => {
 };
 
 const onConnect = (ws) => {
-    clients.add(ws);
+
+    let currentClient = null;
+
+    const broadcastMessage = (message) => {
+        [...clients]
+            .filter(client => client.socket !== ws)
+            .forEach(client => client.socket.send(message));
+    };
+
     console.log("New client: " + ws);
 
-    ws.on('message', (message) => {
-        console.log(message);
-        const targetMessage = attachMessageId(message);
-        const targetMessageString = JSON.stringify(targetMessage);
+    ws.on('message', (rawMessage) => {
+        const parsedMessage = JSON.parse(rawMessage);
+        const username = parsedMessage.username;
 
-        [...clients]
-            .filter(client => client !== ws)
-            .forEach(client => client.send(targetMessageString));
+        switch (parsedMessage.type) {
+            case 'message':
+                const targetMessage = attachMessageId(parsedMessage);
+                const targetMessageString = JSON.stringify(targetMessage);
+                broadcastMessage(targetMessageString);
+                break;
+            case 'user-connect':
+                userId = uuidv4();
+                currentClient = {
+                    username,
+                    socket: ws,
+                    userId
+                };
+
+                clients.add(currentClient);
+
+                broadcastMessage(JSON.stringify({
+                    type: 'user-joined',
+                    userId: currentClient.userId,
+                    username,
+                }));
+
+                break;
+            default:
+                break;
+        }
     });
 
     ws.on('close', () => {
         console.log(ws + ' closed');
-        clients.delete(ws);
+        const { userId, username } = currentClient;
+
+        broadcastMessage(JSON.stringify({
+            type: 'user-left',
+            userId,
+            username
+        }));
+
+        clients.delete(currentClient);
+
     })
 };
 
